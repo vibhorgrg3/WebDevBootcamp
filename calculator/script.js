0dad258aa0460ef6e9f294bd781fec9f640b1fac
vibhor <vibhorgrg3@gmail.com>
2022-07-15 08:12:55 +0530

    adds calculator assignment

---
+++ calculator/script.js
@@ -0,0 +1,71 @@
+$('button').on('click', function () {
+    console.log($('button').text());
+    console.log("fe");
+})
+
+$("#1").on('click', function () {
+    $('input').val($('input').val() + "1");
+})
+$("#2").on('click', function () {
+    $('input').val($('input').val() + "2");
+})
+$("#3").on('click', function () {
+    $('input').val($('input').val() + "3");
+})
+$("#4").on('click', function () {
+    $('input').val($('input').val() + "4");
+})
+$("#5").on('click', function () {
+    $('input').val($('input').val() + "5");
+})
+$("#6").on('click', function () {
+    $('input').val($('input').val() + "6");
+})
+$("#7").on('click', function () {
+    $('input').val($('input').val() + "7");
+})
+$("#8").on('click', function () {
+    $('input').val($('input').val() + "8");
+})
+$("#9").on('click', function () {
+    $('input').val($('input').val() + "9");
+})
+$("#0").on('click', function () {
+    $('input').val($('input').val() + "0");
+})
+
+$("#c").on('click', function () {
+    $('input').val("");
+})
+
+$("#plusOp").on('click', function () {
+    $('input').val($('input').val() + "+");
+})
+
+
+$("#subOp").on('click', function () {
+    $('input').val($('input').val() + "-");
+})
+
+$("#mulOp").on('click', function () {
+    $('input').val($('input').val() + "*");
+})
+
+$("#divOp").on('click', function () {
+    $('input').val($('input').val() + "/");
+})
+$("#modOp").on('click', function () {
+    $('input').val($('input').val() + "%");
+})
+
+$("#eq").on('click', function () {
+    let ans = 0;
+    try {
+        ans = eval($('input').val());
+        $('input').val(ans);
+    } catch (error) {
+        alert("enter valid expression");
+        $('input').val("");
+    }
+
+})
